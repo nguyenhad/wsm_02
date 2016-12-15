04f961470738d7157d514027550bd9cbcc3f6484
Nguyen Huu Giap <nguyenhuugiap@gmail.com>
2016-12-15 14:47:27 +0700

    update delete location gojs
    fake-master-date-request-offs

--- db/migrate/20161214090802_create_compensations.rb
+++ db/migrate/20161214090802_create_compensations.rb
@@ -3,6 +3,7 @@ class CreateCompensations < ActiveRecord::Migration[5.0]
     create_table :compensations do |t|
       t.datetime :from
       t.datetime :to
+      t.references :request_leave, foreign_key: true
       t.integer :status
       t.integer :type
       t.references :request_leave, foreign_key: true
